8a4d6693c76c081b36dbd93c73c5106951f9c008
Szymon Kurek <contact.kurek@gmail.com>
2026-02-15 22:09:25 +0100

    rust: rustlings for functions and ifs + quiz

--- rust/rustlings/exercises/quizzes/quiz1.rs
+++ rust/rustlings/exercises/quizzes/quiz1.rs
@@ -10,7 +10,14 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    let mut price = 2;
+    if apples > 40 {
+        price = 1;
+    }
+
+    apples * price
+}
 
 fn main() {
     // You can optionally experiment here.
