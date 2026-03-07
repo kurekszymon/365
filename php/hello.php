15ed63e51f27b5760fa20f95796522183d9bb57c
Szymon Kurek <contact.kurek@gmail.com>
2026-03-07 20:45:45 +0100

    php: init

---
+++ php/hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Example</title>
+</head>
+
+<body>
+
+    <?php
+    echo "Hi, I'm a PHP script!";
+    echo "!123";
+
+    phpinfo();
+    ?>
+
+</body>
+
+</html>
