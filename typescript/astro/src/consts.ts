b66428f985634b7e8761c23c5c200051170a1792
Szymon Kurek <contact.kurek@gmail.com>
2026-03-12 20:31:40 +0100

    astro: make header not clickable, include linkedin, small improvements

--- typescript/astro/src/consts.ts
+++ typescript/astro/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'kurekszymon';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Szymon Kurek personal website';
