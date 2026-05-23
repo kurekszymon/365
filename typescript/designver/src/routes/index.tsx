9a124ffadf826c86a911f7e23966327e61fb46cf
Szymon Kurek <contact.kurek@gmail.com>
2026-05-23 08:32:39 +0200

    designver: drop project

--- typescript/designver/src/routes/index.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { PresetGallery } from '#/components/editor/PresetGallery'
-
-export const Route = createFileRoute('/')({ component: PresetGallery })
