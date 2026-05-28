300aa0224976742ab71333d58bf5ad88344f820b
Szymon Kurek <contact.kurek@gmail.com>
2026-05-29 01:31:19 +0200

    offboard: displat employee data

--- typescript/offboard/src/app/employee-details/employee-details.ts
+++ typescript/offboard/src/app/employee-details/employee-details.ts
@@ -7,18 +7,24 @@ import {
   viewChild,
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
   imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './employee-details.html',
+  styleUrl: './employee-details.css',
 })
 export class EmployeeDetails {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private employeeService = inject(EmployeeService);
 
-  protected id = this.route.snapshot.paramMap.get('id');
+  protected id = this.route.snapshot.paramMap.get('id')!;
+
+  protected employee = toSignal(this.employeeService.getEmployee(this.id));
 
   private dialogRef = viewChild<ElementRef<HTMLDialogElement>>('dialog');
 
