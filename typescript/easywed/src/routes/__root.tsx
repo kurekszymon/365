c419ca76fa394195d982e65d49cabe5e97a80ae2
Szymon Kurek <contact.kurek@gmail.com>
2026-06-07 23:27:09 +0200

    easywed: replace __root title

--- typescript/easywed/src/routes/__root.tsx
+++ typescript/easywed/src/routes/__root.tsx
@@ -41,7 +41,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "EasyWed - wedding planner app",
+        title: "easywed.",
       },
     ],
     links: [
